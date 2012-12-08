465c297e597149b0ec43a52ebb578369ba7835d9
Rafał Tulwin <rtulwin@gmail.com>
2012-12-08 16:41:17 +0100

    adding custom segmeneted control empty files

---
+++ KSCustomSegmentedControl/KSCustomSegmentedControl.h
@@ -0,0 +1,13 @@
+//
+//  KSCustomSegmentedControl.h
+//  KSCustomGraphicsUtils
+//
+//  Created by Rafał Tulwin on 08.12.2012.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KSCustomSegmentedControl : UIView
+
+@end
---
+++ KSCustomSegmentedControl/KSCustomSegmentedControl.m
@@ -0,0 +1,26 @@
+//
+//  KSCustomSegmentedControl.m
+//  KSCustomGraphicsUtils
+//
+//  Created by Rafał Tulwin on 08.12.2012.
+//
+//
+
+#import "KSCustomSegmentedControl.h"
+
+@implementation KSCustomSegmentedControl
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+    
+}
+
+@end
